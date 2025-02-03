50cd96a30ef6c191e421cc59dbc583f22dfe09db
Carlos Arroyo Ruiz <carlos.arroyo.ruiz@upm.es>
2025-02-03 12:41:19 +0000

    Delete UNet_train.py

--- Convolutional_NN_PCB/UNet_train.py
+++
@@ -1,7 +0,0 @@
-from UNet_train_functions import *
-
-n_train_cases = [50,100,250,500,1000,2000,5000]
-
-for nt in n_train_cases:
-    UNET_train(epochs=500,n_train=nt,overwrite=True)
-    UNET_train_physics(epochs=500,n_train=nt,phy_param=1.0,overwrite=True)
