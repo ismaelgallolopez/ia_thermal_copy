bfed06785bcb8b74503b49e4241517b5cbc8b693
Carlos Arroyo Ruiz <carlos.arroyo.ruiz@upm.es>
2025-02-03 12:41:01 +0000

    Delete UNet_finetuning_train.py

--- Convolutional_NN_PCB/UNet_finetuning_train.py
+++
@@ -1,9 +0,0 @@
-from UNet_finetuning_train_functions import *
-
-n_train_cases = [50,100,250,500,1000,2000,5000]
-n_train_finetuning_cases = [10,20,30,40,50,60,70,80,90,100]
-
-for nt in n_train_cases:
-    for nt2 in n_train_finetuning_cases:
-        UNET_finetuning_train(pretrained_model='case_e_500_nt_{}_bs_5'.format(nt),epochs=500,
-                              n_train=nt2,lr0=1E-6,overwrite=True,batch_size=1)
