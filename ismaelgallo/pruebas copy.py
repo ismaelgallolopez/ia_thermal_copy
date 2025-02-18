3fd27229e9d3b5479524de6992c1b3c681afe99c
ismael <ismael.lopez@alumnos.upm.es>
2025-02-18 08:08:51 +0100

    Generación datasets

--- ismaelgallo/pruebas copy.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-
-print("Versión de PyTorch:", torch.__version__)
-print("CUDA disponible:", torch.cuda.is_available())
-if torch.cuda.is_available():
-    print("Versión de CUDA:", torch.version.cuda)
-    print("Nombre de la GPU:", torch.cuda.get_device_name(0))
--- ismaelgallo/pruebas.py
+++ ismaelgallo/pruebas.py
@@ -7,3 +7,9 @@ print("Python version:", sys.version)
 print("sys.prefix:", sys.prefix)
 print("CONDA_DEFAULT_ENV:", os.environ.get("CONDA_DEFAULT_ENV"))
 print("CUDA available:", torch.cuda.is_available())
+
+print("Versión de PyTorch:", torch.__version__)
+print("CUDA disponible:", torch.cuda.is_available())
+if torch.cuda.is_available():
+    print("Versión de CUDA:", torch.version.cuda)
+    print("Nombre de la GPU:", torch.cuda.get_device_name(0))
