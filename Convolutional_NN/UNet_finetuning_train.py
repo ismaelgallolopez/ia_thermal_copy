e3e8450b271ee1484a3ea4370ca64d2a9d6afa23
ismael <ismael.lopez@alumnos.upm.es>
2025-02-24 12:00:00 +0100

    Creación de imágenes de recurso

--- Convolutional_NN/UNet_finetuning_train.py
+++ Convolutional_NN/UNet_finetuning_train.py
@@ -6,4 +6,4 @@ n_train_finetuning_cases = [10,20,30,40,50,60,70,80,90,100]
 for nt in n_train_cases:
     for nt2 in n_train_finetuning_cases:
         UNET_finetuning_train(pretrained_model='case_e_500_nt_{}_bs_5'.format(nt),epochs=500,
-                              n_train=nt2,lr0=1E-6,overwrite=True,batch_size=1)
+                              n_train=nt2,lr0=1E-6,overwrite=True,batch_size=1) 
